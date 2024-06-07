11c8a149c6094c6089c0d8b88ae0e29055d9ffc6
Rafael Santos <rafa.05125@gmail.com>
2024-06-07 15:54:40 -0300

    IDVDService, DVDService

---
+++ DVDRentalAPI/Domain/Interfaces/IDVDService.cs
@@ -0,0 +1,16 @@
+﻿using DVDRentalAPI.Domain.Entities;
+
+namespace DVDRentalAPI.Domain.Interfaces
+{
+    public interface IDVDService
+    {
+        List<DVD> GetAllDVDs(int page = 1, string? title = null, 
+                            string? genre = null, int? duration = null, 
+                            int? year = null, DateTime? releaseDate = null);
+
+        DVD? FindById(int id);
+        void Create(DVD dvd);
+        void Update(DVD dvd);
+        void Delete(int id);
+    }
+}
---
+++ DVDRentalAPI/Services/DVDService.cs
@@ -0,0 +1,55 @@
+﻿using DVDRentalAPI.Data;
+using DVDRentalAPI.Domain.Entities;
+using DVDRentalAPI.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DVDRentalAPI.Services
+{
+    public class DVDService : IDVDService
+    {
+        private readonly SQLContext _context;
+
+        public DVDService(SQLContext context)
+        {
+            _context = context;
+        }
+
+        public List<DVD> GetAllDVDs(int page = 1, string? title = null, string? genre = null, int? duration = null, int? year = null, DateTime? releaseDate = null)
+        {
+            var query = _context.Dvds.AsQueryable();
+            if (!string.IsNullOrEmpty(title))
+            {
+                query = query.Where(d => EF.Functions.Like(d.Title.ToLower(), $"%{title}%"));
+            }
+
+            int itensPerPage = 10;
+
+            query = query.Skip((page - 1) * itensPerPage).Take(itensPerPage);
+
+            return [.. query];
+        }
+
+        public DVD? FindById(int id)
+        {
+            return _context.Dvds.Where(v => v.Id == id).FirstOrDefault();
+        }
+
+        public void Create(DVD dvd)
+        {
+            _context.Dvds.Add(dvd);
+            _context.SaveChanges();
+        }
+
+        public void Update(DVD dvd)
+        {
+            _context.Dvds.Update(dvd);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            _context.Remove(id);
+            _context.SaveChanges();
+        }
+    }
+}
