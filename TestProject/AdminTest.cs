0c9ecc6b8a3d7252cf77e4ec8828fc68ec5b42de
Rafael Santos <rafa.05125@gmail.com>
2024-06-09 13:05:49 -0300

    Initial tests

---
+++ TestProject/AdminTest.cs
@@ -0,0 +1,28 @@
+using DVDRentalAPI.Domain.Entities;
+
+namespace TestProject
+{
+    [TestClass]
+    public class AdminTest
+    {
+        [TestMethod]
+        public void GetSetPropertiesTest()
+        {
+
+            //Arrange
+            var adm = new Admin();
+
+            //Act
+            adm.Id = 1;
+            adm.Email = "test@test.com";
+            adm.Password = "password";
+            adm.Profile = "Adm";
+
+            //Assert
+            Assert.AreEqual(1, adm.Id);
+            Assert.AreEqual("test@test.com", adm.Email);
+            Assert.AreEqual("password", adm.Password);
+            Assert.AreEqual("Adm", adm.Profile);
+        }
+    }
+}
